42fe1cc36eb10445bd71b36cbf8b1d408e5c8f6a
zain <paryanizain@gmail.com>
2024-12-01 09:13:59 +0500

    changes

--- class-11-hm/my-app/app/components/CarsArray.tsx
+++ class-11-hm/my-app/app/components/CarsArray.tsx
@@ -1,24 +1,55 @@
-import Image from "next/image"; 
+import Image from "next/image";
 import React from "react";
+
 const CarsArray = () => {
   const carsName = [
-    { carname: "Alto", carprice: 2800000, image: <Image src="/alto1.jpg" alt="Suzuki Alto" height={300} width={300}/> },
-    { carname: "City", carprice: 7400000, image: <Image src="/city1.jpg" alt="Honda City" height={300} width={300}/> },
-    { carname: "Juke", carprice: 6300000, image: <Image src="/juke1.png" alt="Nissan Juke" height={300} width={300}/> },
+    {
+      carname: "Alto",
+      carprice: 2800000,
+      image: "/alto1.jpg",
+    },
+    {
+      carname: "City",
+      carprice: 7400000,
+      image: "/city1.jpg",
+    },
+    {
+      carname: "Juke",
+      carprice: 6300000,
+      image: "/juke1.png",
+    },
   ];
+
   return (
     <div>
-      {carsName.map((cn , index) => {
-        return (
-          <div className="bg-slate-300 flex items-center justify-center gap-2 py-12">
-            <div key={index}  className="bg-blue-300 flex-col md:flex-row w-3/12 text-center p-4">
-              {cn.image}
-              <h1 className="text-black text-base font-semibold">{cn.carname}</h1>
-              <p className="text-black text-base font-semibold">{cn.carprice}</p>
-            </div>
+      {carsName.map((cn, index) => (
+        <div
+          key={index}
+          className="bg-slate-300 flex items-center justify-center gap-2 py-12"
+        >
+          <div className="bg-blue-300 flex flex-col md:flex-row w-3/12 text-center p-4">
+            <Image
+              src={cn.image}
+              alt={cn.carname}
+              height={300}
+              width={300}
+              className="object-cover"
+            />
+            <h1
+              key={`name-${index}`}
+              className="text-black text-base font-semibold"
+            >
+              {cn.carname}
+            </h1>
+            <p
+              key={`price-${index}`}
+              className="text-black text-base font-semibold"
+            >
+              {cn.carprice.toLocaleString()}
+            </p>
           </div>
-        );
-      })}
+        </div>
+      ))}
     </div>
   );
 };
